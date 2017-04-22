ac57167f143dc62fe500a25d7d10faf770e1c146
Beau Barker <beau@barkerest.com>
2017-04-22 19:25:32 -0400

    Change is_const line to better handle the call to const_defined?.

--- lib/shells.rb
+++ lib/shells.rb
@@ -22,7 +22,16 @@ module Shells
   # Provides the ability for the Shells module to allow sessions to be instantiated without calling +new+.
   def self.method_missing(m, *args, &block)  #:nodoc:
 
-    is_const = const_defined?(m) rescue nil
+    is_const =
+        if m.to_s =~ /^[A-Z][a-zA-Z0-9_]*$/ # must start with uppercase and contain only letters, numbers, and underscores.
+          begin
+            const_defined?(m)
+          rescue NameError  # if for some reason we still get a NameError, it's obviously not a constant.
+            false
+          end
+        else
+          false
+        end
 
     if is_const
       val = const_get(m)
--- lib/shells/version.rb
+++ lib/shells/version.rb
@@ -1,5 +1,5 @@
 module Shells
   ##
   # The current version of the gem.
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
